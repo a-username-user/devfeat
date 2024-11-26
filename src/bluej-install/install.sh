eb36945237d29910dca44b070ca78466b1103415
a-username-user <117302494+a-username-user@users.noreply.github.com>
2024-11-26 15:12:14 +0100

    Update install.sh

--- src/bluej-install/install.sh
+++ src/bluej-install/install.sh
@@ -1,1 +1,9 @@
+# Make sure JDK 21 is installed
+apt-get update
+apt-get install -y --no-install-recommends jdk-21
 
+# Get BlueJ
+FILENAME="BlueJ-linux-x64-541.deb"
+cd /tmp
+wget https://www.bluej.org/download/files/"${FILENAME}"
+apt install ./"${FILENAME}"
